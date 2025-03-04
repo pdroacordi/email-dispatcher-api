1222988822964dfd4f0a9892ee2fc67ee1498e7c
Pedro Acordi <pedroacordi2@gmail.com>
2025-03-04 09:43:15 -0300

    feat: add email configuration

---
+++ src/main/java/io/acordi/emaildispatchermicroservice/config/MailConfig.java
@@ -0,0 +1,43 @@
+package io.acordi.emaildispatchermicroservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Value("${acordi.mail.host}")
+    private String host;
+
+    @Value("{acordi.mail.port}")
+    private int port;
+
+    @Value("{acordi.mail.username}")
+    private String username;
+
+    @Value("{acordi.mail.password}")
+    private String password;
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        return mailSender;
+    }
+
+}
