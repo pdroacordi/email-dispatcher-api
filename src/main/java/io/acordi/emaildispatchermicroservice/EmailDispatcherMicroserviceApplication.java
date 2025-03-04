9bcaec4effa6511950565c6ab58703e09466476e
Pedro Acordi <pedroacordi2@gmail.com>
2025-03-04 09:09:48 -0300

    project setup

---
+++ src/main/java/io/acordi/emaildispatchermicroservice/EmailDispatcherMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package io.acordi.emaildispatchermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailDispatcherMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailDispatcherMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/acordi/emaildispatchermicroservice/EmailDispatcherMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.acordi.emaildispatchermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailDispatcherMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
